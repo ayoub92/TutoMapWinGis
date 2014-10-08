6721d342ebccbb43cd5af80067c06a07c4c818a8
Smigal Target <qt.target@gmail.com>
2014-10-08 19:13:19 +0100

    premier version du projet MapWinGis

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMap1 = new AxMapWinGIS.AxMap();
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMap1
+            // 
+            this.axMap1.Enabled = true;
+            this.axMap1.Location = new System.Drawing.Point(153, 58);
+            this.axMap1.Name = "axMap1";
+            this.axMap1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMap1.OcxState")));
+            this.axMap1.Size = new System.Drawing.Size(449, 304);
+            this.axMap1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 474);
+            this.Controls.Add(this.axMap1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axMap1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMapWinGIS.AxMap axMap1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MapWinGIS.Shapefile sp = new MapWinGIS.Shapefile();
+            sp.Open("./GISSampleData/base.shp", null);
+            axMap1.AddLayer(sp, true);
+            MapWinGIS.Shapefile sp1 = new MapWinGIS.Shapefile();
+            sp1.Open("./GISSampleData/nile.shp", null);
+            
+            axMap1.AddLayer(sp1, true);
+
+        }
+    }
+}
